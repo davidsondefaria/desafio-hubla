2dfd0a930132c6237ab48514c86ee1f86d00d813
Davidson <davidsondefaria@gmail.com>
2023-10-07 20:28:32 -0300

    feat: Creating the transaction entity aslong with typeorm configs

--- backend/src/transactions/entities/transaction.entity.ts
+++ backend/src/transactions/entities/transaction.entity.ts
@@ -1,1 +1,39 @@
-export class Transaction {}
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Unique(['id'])
+@Entity('transactions')
+export class Transaction extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ nullable: false })
+  type: number; // TODO create a TransactionType
+
+  @Column({ nullable: false })
+  date: string; // TODO Deal this as Date
+
+  @Column({ nullable: false })
+  product: string;
+
+  @Column({ nullable: false })
+  value: number;
+
+  @Column({ nullable: false })
+  vendor: string;
+
+  @Column({ nullable: false })
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Column({ nullable: false })
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
