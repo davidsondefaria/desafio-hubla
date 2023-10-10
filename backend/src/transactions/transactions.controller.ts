3147937245db2aceb1a4a17624fc5037419f14e7
Davidson <davidsondefaria@gmail.com>
2023-10-10 19:18:05 -0300

    feat: Adding types and interfaces

--- backend/src/transactions/transactions.controller.ts
+++ backend/src/transactions/transactions.controller.ts
@@ -1,7 +1,15 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
+import { ApiBody, ApiConsumes, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Express } from 'express';
+
 import { TransactionsService } from './transactions.service';
-import { CreateTransactionDto } from './dto/create-transaction.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { FileUploadDto } from './dto/file-transaction-upload.dt';
 
 @Controller('transactions')
 @ApiTags('Transactions')
@@ -9,27 +17,17 @@ export class TransactionsController {
   constructor(private readonly transactionsService: TransactionsService) {}
 
   @Post()
-  create(@Body() createTransactionDto: CreateTransactionDto) {
-    return this.transactionsService.create(createTransactionDto);
+  @UseInterceptors(FileInterceptor('file'))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    description: 'List of transactions',
+    type: FileUploadDto,
+  })
+  @ApiOkResponse({
+    description: 'The file was submitted successfully',
+    // type: , //TODO define this type
+  })
+  readTransactionsFile(@UploadedFile() file: Express.Multer.File) {
+    return this.transactionsService.transformBuffer(file.buffer);
   }
-
-  // @Get()
-  // findAll() {
-  //   return this.transactionsService.findAll();
-  // }
-
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.transactionsService.findOne(+id);
-  // }
-
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateTransactionDto: UpdateTransactionDto) {
-  //   return this.transactionsService.update(+id, updateTransactionDto);
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.transactionsService.remove(+id);
-  // }
 }
